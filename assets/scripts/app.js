f5b98aa1191f99418f8e7317ad2077670fc20fdb
imSogand <imsogand@outlook.com>
2024-10-10 13:26:54 +0200

    adding subtract and createAndWriteOutput function(more dynamic)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,29 @@ function getUserNumberInput() {
   return parseInt(usrInput.value);
 }
 
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult}+${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCal, calNumber) {
+  const calcDescription = `${resultBeforeCal}${operator}${calNumber}`;
   outputResult(currentResult, calcDescription);
+
 }
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+ 
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-',initialResult, enteredNumber);
+}
+
+function multiply() {}
+
+function divide() {}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
